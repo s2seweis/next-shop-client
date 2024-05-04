3eb998a62e716f7e9f6a34ea8381a4c207feea35
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2024-05-04 14:50:27 +0200

    removed error

--- src/pages/reset/SuccessMessage.tsx
+++ src/pages/reset/SuccessMessage.tsx
@@ -5,10 +5,10 @@ const ResetMessage: React.FC = () => {
   return (
     <div style={{ width: "100%" }}>
       <div style={{ background: 'aliceblue', padding: '30px', margin: '250px auto', width: 'fit-content', borderRadius: '25px' }}>
-      <div style={{ margin: '15px 0px 0px 15px', display: 'flex' }}>
-        <a href='javascript:history.back()'>Go Back</a>
-      </div>
-        <h2 style={{ padding: '10px' }}>You changed your password succesful!"</h2>
+        <div style={{ margin: '15px 0px 0px 15px', display: 'flex' }}>
+          <a href='javascript:history.back()'>Go Back</a>
+        </div>
+        <h2 style={{ padding: '10px' }}>You changed your password successfully!</h2>
         <h4>Go back to the Sign In Page</h4>
       </div>
     </div>
